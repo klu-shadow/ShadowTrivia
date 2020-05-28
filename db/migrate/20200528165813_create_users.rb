4a5c3542c3aaa1a9ebec42074e9d6d2f6a241d38
klu-sh <kevin.lu@shadowhealth.com>
2020-05-28 13:08:43 -0400

    Add users table

---
+++ db/migrate/20200528165813_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :role, default: "standard"
+      t.timestamps
+      
+      t.index :email, unique: true
+      t.index :password_digest, unique: true
+      t.index :session_token, unique: true
+      t.index :first_name
+      t.index :last_name
+    end
+  end
+end
